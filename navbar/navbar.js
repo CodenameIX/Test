db0f06f3a51b463f0a811d7bb263df1070c2ac8b
CodenameIX <Codename_IX@gmx.de>
2023-02-28 13:28:28 +0100

    .

---
+++ navbar/navbar.js
@@ -0,0 +1,20 @@
+const navbar = document.querySelector('.topnavbar0')
+fetch('navbar/navbar.html')
+    .then(res => res.text())
+    .then(data => {
+        navbar.innerHTML = data
+        const parser = new DOMParser()
+        const doc = parser.parseFromString(data, 'text/html')
+    }).then(()=>{
+        document.getElementById("topnavbar1").onmousemove = e => {
+            for (const topcard1 of document.getElementsByClassName("topcard1")) {
+                const rect = topcard1.getBoundingClientRect(),
+                    x = e.clientX - rect.left,
+                    y = e.clientY - rect.top;
+    
+                topcard1.style.setProperty("--mouse-x", `${x}px`);
+                topcard1.style.setProperty("--mouse-y", `${y}px`);
+            };
+        }
+    })
+
